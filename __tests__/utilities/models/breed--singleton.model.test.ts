5b18cf47171bd66aab9302244ef8d374948ca2d7
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-20 14:40:54 +0530

    test(utilities--model): unit tests for BreedSingleton

---
+++ __tests__/utilities/models/breed--singleton.model.test.ts
@@ -0,0 +1,19 @@
+import { BreedSingletonModel } from 'utilities/models';
+import { apiGetRequest } from 'mocks';
+
+describe('BreedSingletonModel', () => {
+  apiGetRequest('breed/african/images', {
+    status: 'success',
+    message: ['https://images.dog.ceo/breeds/african/n02116738_10024.jpg']
+  });
+  const singleton = BreedSingletonModel.create({ id: 'african' });
+
+  it('should create without any issues', async () => {
+    expect(singleton.state).toBe('idle');
+
+    await singleton.fetchImages()
+
+    expect(singleton.state).toBe('success')
+    expect(singleton.images.length).toBeGreaterThan(0)
+  });
+});
