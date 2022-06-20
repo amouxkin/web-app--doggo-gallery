b12b7a6259102618ec737b4d25745177238a6eea
Ananthu <ananthusudhikumar@gmail.com>
2022-06-21 05:08:56 +0530

    feat(pages--home): add images loading animation and badges

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { observer } from 'mobx-react-lite';
 import { motion, AnimatePresence } from 'framer-motion';
-import { HStack, Image, VStack, Wrap, WrapItem } from '@chakra-ui/react';
+import { Badge, HStack, Image, Spinner, Stack, VStack, Wrap, WrapItem } from '@chakra-ui/react';
 import { BreedParentModelInstance } from 'utilities/models';
 import { useBreedStore } from 'store';
 import { BreedSingleCheckBox } from 'components/atoms';
@@ -25,17 +25,42 @@ export const Home: FC = observer(() => {
       </VStack>
       <Wrap>
         <AnimatePresence>
-          {images.map((image) => (
-            <WrapItem
-              as={motion.div}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              key={image}
-            >
-              <Image boxSize="200px" objectFit="cover" src={image} alt={`image`} />
-            </WrapItem>
-          ))}
+          {images.map((image) => {
+            // TODO: change image from string to model.
+            const [breed, subBreed] = image.split('/').slice(4)[0].split('-');
+            return (
+              <WrapItem
+                as={motion.div}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                key={image}
+              >
+                <VStack>
+                  <Image
+                    boxSize="200px"
+                    objectFit="cover"
+                    fallback={
+                      <Stack
+                        h={'200px'}
+                        w={'200px'}
+                        justifyContent={'center'}
+                        alignItems={'center'}
+                      >
+                        <Spinner />
+                      </Stack>
+                    }
+                    src={image}
+                    alt={`image`}
+                  />
+                  <HStack>
+                    <Badge colorScheme="green">{breed}</Badge>
+                    <Badge colorScheme="purple">{subBreed}</Badge>
+                  </HStack>
+                </VStack>
+              </WrapItem>
+            );
+          })}
         </AnimatePresence>
       </Wrap>
     </HStack>
