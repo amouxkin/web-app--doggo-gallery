820a9b82f295dd8a9bdad40f24f665c43b0c0d45
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-20 14:38:03 +0530

    chore(utilities--models): export all breed models

--- src/utilities/models/index.ts
+++ src/utilities/models/index.ts
@@ -1,1 +1,5 @@
 export * from './api-state.model';
+export * from './breed--base.model';
+export * from './breed--parent.model';
+export * from './breed--sub.model';
+export * from './breed--singleton.model';
