43a849c64011fb70d9521170c65ba0de5d6bf71b
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-21 00:42:27 +0530

    test(store): add basic tests for categories

--- __tests__/store/categories.store.test.ts
+++ __tests__/store/categories.store.test.ts
@@ -1,20 +1,54 @@
 import { CategoriesStore } from 'store';
 import { apiGetRequest } from 'mocks';
 import { expect } from 'vitest';
+import { runInAction } from 'mobx';
+import { skipCycle } from '../__mocks__/cycle';
 
 describe('CategoriesStore', () => {
   apiGetRequest('breeds/list', {
     status: 'success',
     message: {
       appenzeller: [],
-      australian: ['shepherd']
+      cattledog: ['australian']
     }
   });
 
+  apiGetRequest('breed/appenzeller/images', {
+    status: 'success',
+    message: ['https://images.dog.ceo/breeds/appenzeller/n02107908_1575.jpg']
+  });
+
+  apiGetRequest('breed/cattledog/australian/images', {
+    status: 'success',
+    message: ['https://images.dog.ceo/breeds/cattledog-australian/IMG_4386.jpg']
+  });
+
+  const store = CategoriesStore.create();
+
   it('should fetch categories correctly', async () => {
-    const store = CategoriesStore.create();
     await store.fetchCategories();
 
     expect(store.state).toBe('success');
+  });
+
+  it('should fetch nothing if no image is selected', async () => {
+    await store.fetchSelectedImages();
+    expect(store.categories[0].images).toStrictEqual([]);
+    expect(store.categories[1].images).toStrictEqual([]);
+  });
+
+  it('should fetch selected singleton', async () => {
+    store.categories[0].select();
+    await runInAction(store.fetchSelectedImages);
+
+    /**
+     * Two cycles for mocks to be resolved.
+     */
+    await skipCycle();
+    await skipCycle();
+
+    expect(store.categories[0].state).toBe('success');
+    expect(store.categories[0].images.length).toBe(1);
+    expect(store.categories[1].images.length).toBe(0);
   });
 });
