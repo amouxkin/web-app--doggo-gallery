83583c37b89b82a582a9bcb4fab5e90b6efcd5fe
Ananthu <ananthusudhikumar@gmail.com>
2022-06-21 05:27:31 +0530

    feat(pages--home): add input selector

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
 import { observer } from 'mobx-react-lite';
 import { motion, AnimatePresence } from 'framer-motion';
-import { Badge, HStack, Image, Spinner, Stack, VStack, Wrap, WrapItem } from '@chakra-ui/react';
+import {
+  Badge,
+  Box,
+  HStack,
+  Image,
+  Spinner,
+  Stack,
+  VStack,
+  Wrap,
+  WrapItem
+} from '@chakra-ui/react';
 import { BreedParentModelInstance } from 'utilities/models';
 import { useBreedStore } from 'store';
 import { BreedSingleCheckBox } from 'components/atoms';
 import { BreedWithSubCheckBox } from 'components/molecules';
+import { Select } from 'chakra-react-select';
 
 export const Home: FC = observer(() => {
   const { categories, images } = useBreedStore();
@@ -23,46 +34,72 @@ export const Home: FC = observer(() => {
           )
         )}
       </VStack>
-      <Wrap>
-        <AnimatePresence>
-          {images.map((image) => {
-            // TODO: change image from string to model.
-            const [breed, subBreed] = image.split('/').slice(4)[0].split('-');
-            return (
-              <WrapItem
-                as={motion.div}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                key={image}
-              >
-                <VStack>
-                  <Image
-                    boxSize="200px"
-                    objectFit="cover"
-                    fallback={
-                      <Stack
-                        h={'200px'}
-                        w={'200px'}
-                        justifyContent={'center'}
-                        alignItems={'center'}
-                      >
-                        <Spinner />
-                      </Stack>
-                    }
-                    src={image}
-                    alt={`image`}
-                  />
-                  <HStack>
-                    <Badge colorScheme="green">{breed}</Badge>
-                    <Badge colorScheme="purple">{subBreed}</Badge>
-                  </HStack>
-                </VStack>
-              </WrapItem>
-            );
-          })}
-        </AnimatePresence>
-      </Wrap>
+      <VStack>
+        <HStack>
+          <Box>
+            <Select
+              options={categories.reduce((options, category) => {
+                options.push({
+                  label: category.name,
+                  value: category.name
+                });
+
+                (category as BreedParentModelInstance)?.subBreeds?.forEach((subBreed) =>
+                  options.push({
+                    label: `${category.name}-${subBreed.name}`,
+                    value: `${category.name}-${subBreed.name}`
+                  })
+                );
+
+                return options;
+              }, [] as Array<{ label: string; value: string }>)}
+              isMulti
+              placeholder={'Enter a breed name'}
+              closeMenuOnSelect={false}
+            />
+          </Box>
+        </HStack>
+        <Wrap>
+          <AnimatePresence presenceAffectsLayout>
+            {images.map((image) => {
+              // TODO: change image from string to model.
+              const [breed, subBreed] = image.split('/').slice(4)[0].split('-');
+              return (
+                <WrapItem
+                  as={motion.div}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  key={image}
+                >
+                  <VStack>
+                    <Image
+                      boxSize="200px"
+                      objectFit="cover"
+                      fallback={
+                        <Stack
+                          h={'200px'}
+                          w={'200px'}
+                          justifyContent={'center'}
+                          alignItems={'center'}
+                        >
+                          <Spinner />
+                        </Stack>
+                      }
+                      src={image}
+                      alt={`image`}
+                    />
+                    <HStack>
+                      <Badge colorScheme="green">{breed}</Badge>
+                      <Badge colorScheme="purple">{subBreed}</Badge>
+                    </HStack>
+                  </VStack>
+                </WrapItem>
+              );
+            })}
+          </AnimatePresence>
+        </Wrap>
+      </VStack>
     </HStack>
   );
 });
