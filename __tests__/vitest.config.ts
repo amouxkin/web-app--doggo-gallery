75f78336fb544c7635de455f0cd80c77297b4876
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-20 21:28:27 +0530

    refactor(test--mocks): move mse mocking to global.

--- __tests__/vitest.config.ts
+++ __tests__/vitest.config.ts
@@ -1,3 +1,10 @@
 import nodeFetch from 'node-fetch';
+import { setupServer } from 'msw/node';
 
 global.fetch = nodeFetch as Window['fetch'];
+
+export const server = setupServer();
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error'}));
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
