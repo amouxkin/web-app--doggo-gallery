4e95ccbb4b8229cf113683d4e86df2751fc045c7
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-20 15:20:29 +0530

    chore(test): add mocking for fetch and change env to jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import tsConfigPath from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: '__tests__/vitest.config.ts'
   },
   plugins: [react(), tsConfigPath()]
 });
