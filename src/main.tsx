ab1f7f06d9e9cc9b3bfc8e0875b109672b6acd18
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-21 01:58:30 +0530

    feat(main): wrap root with BreedStore

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { App } from './App';
+import { BreedStoreProvider } from 'store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <ChakraProvider>
+  <React.StrictMode>
+    <ChakraProvider>
+      <BreedStoreProvider>
         <App />
-      </ChakraProvider>
-    </React.StrictMode>
+      </BreedStoreProvider>
+    </ChakraProvider>
+  </React.StrictMode>
 );
