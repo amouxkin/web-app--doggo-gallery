555696a2273b028c4edc836e53cc1a83d403f5c4
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-21 23:56:28 +0530

    feat(components--templates): add Select to BreedSelector

--- src/components/templates/BreedSelector.tsx
+++ src/components/templates/BreedSelector.tsx
@@ -1,28 +1,60 @@
+import { useState } from 'react';
 import { observer } from 'mobx-react-lite';
+import { Button, HStack } from '@chakra-ui/react';
 import { Select } from 'chakra-react-select';
 import {
-  BreedParentModelInstance,
-  BreedSingletonModelInstance,
-  BreedSubModelInstance
+  ChildCategoryInstance,
+  ParentCategoryInstance,
+  SingletonCategoryInstance
 } from 'utilities/models';
 import { useBreedStore } from 'store';
-import { useState } from 'react';
-import { Button, HStack } from '@chakra-ui/react';
-
-export type BreedSelectorValues = {
-  label: string;
-  value: BreedParentModelInstance | BreedSingletonModelInstance;
-};
 
 export const BreedSelector = observer(() => {
-  const { categories } = useBreedStore();
+  const { categoriesAsOptions } = useBreedStore();
 
   const [values, setValues] = useState<
-    Array<BreedParentModelInstance | BreedSubModelInstance | BreedSingletonModelInstance>
+    Array<{
+      label: string;
+      value: ChildCategoryInstance | SingletonCategoryInstance | ParentCategoryInstance;
+    }>
   >([]);
 
   return (
     <HStack>
+      <Select
+        size="md"
+        chakraStyles={{
+          input: (provided) => ({
+            ...provided,
+            minW: '200px'
+          })
+        }}
+        options={categoriesAsOptions}
+        isMulti
+        placeholder={'Enter a category name'}
+        closeMenuOnSelect={false}
+        value={values}
+        onChange={(newValue) => {
+          if (newValue.length > 5) return;
+          setValues((prevState) => {
+            prevState.forEach((child) => {
+              if (!newValue.includes(child)) child.value.unSelect();
+            });
+            newValue.forEach((child) => child.value.select());
+            return newValue as Array<{
+              label: string;
+              value: ChildCategoryInstance | SingletonCategoryInstance | ParentCategoryInstance;
+            }>;
+          });
+        }}
+      />
+      <Button
+        onClick={() => {
+          values.forEach((value) => {});
+        }}
+      >
+        Search
+      </Button>
     </HStack>
   );
 });
