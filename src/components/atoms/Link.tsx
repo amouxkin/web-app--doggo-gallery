cf46d501682c832e116660240586810ac98e757a
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-21 13:13:42 +0530

    feat(components--atoms): add animation and match checker logic

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,5 +1,33 @@
 import { FC } from 'react';
 import { Link as ChakraLink } from '@chakra-ui/react';
-import { Link as WouterLink, LinkProps } from 'wouter';
+import { Link as WouterLink, LinkProps, useRoute } from 'wouter';
 
-export const Link: FC<LinkProps> = (props) => <ChakraLink as={WouterLink} {...props} />;
+export const Link: FC<LinkProps> = (props) => {
+  const [match] = useRoute(props.to || '#');
+  return (
+    <ChakraLink
+      as={WouterLink}
+      fontFamily={'body'}
+      fontWeight={'medium'}
+      textDecoration={'none'}
+      transition={{
+        shortEaseOut: 'all .25s ease-out'
+      }}
+      color={match ? 'gray.600' : 'gray.400'}
+      _active={{
+        textDecoration: 'none'
+      }}
+      _focus={{
+        boxShadow: 'none'
+      }}
+      _hover={{
+        textDecoration: 'none',
+        color: 'gray.900'
+      }}
+      _visited={{
+        textDecoration: 'none'
+      }}
+      {...props}
+    />
+  );
+};
