c43587ee29011fe297981906b701bcff8ca4db1c
Ananthu Sudhikumar <ananthusudhikumar@gmail.com>
2022-06-22 18:25:45 +0530

    feat(components--templates): add Doggo image and align navigation header

--- src/components/templates/NavigationBar.tsx
+++ src/components/templates/NavigationBar.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
-import { HStack } from '@chakra-ui/react';
+import { Heading, HStack, Image } from '@chakra-ui/react';
 import { Link } from 'components/atoms';
+import doggoImage from '../../ions/images/doggo.jpg';
 
 export const NavigationBar: FC = () => (
-    <HStack gap={2} as={'nav'}>
+  <HStack gap={2} as={'nav'} alignItems={'center'} justifyContent={'space-between'} px={20}>
+    <Image src={doggoImage} w={40} />
+    <Heading>Doggo Gallery</Heading>
+    <HStack>
       <Link to={'/'}> Home </Link>
       <Link to={'/selector'}> Selector </Link>
       <Link to={'/saved'}> Saved Images </Link>
     </HStack>
-  );
+  </HStack>
+);
